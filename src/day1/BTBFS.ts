5f39c3457f9c00cfca81ae873d5486956d393681
Victormrf <vmrf2000@hotmail.com>
2025-02-06 17:06:30 -0300

    Studied binary trees

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,22 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head]; //queue of
 
+    while (q.length) {
+        const curr = q.shift() as BinaryNode<number>;
+
+        // search
+        if (curr?.value === needle) {
+            return true;
+        }
+
+        // if not found we keep traverseling
+        if (curr.left) {
+            q.push(curr.left);
+        }
+        if (curr.right) {
+            q.push(curr.right);
+        }
+    }
+
+    return false;
 }
--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,19 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // Base case
+    if (a === null && b === null) {
+        return true;
+    }
 
+    if (a === null || b === null) {
+        return false;
+    }
+
+    if (a === null || b === null) {
+        return true;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
